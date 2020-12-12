60dc72c415eb98e4a58df5d9b14a43e78e52c5e8
mmporong <74720422+mmporong@users.noreply.github.com>
2020-12-12 17:38:02 +0900

    Add files via upload

--- Scripts/Scirpt/HelloCSharp.cs
+++ Scripts/Scirpt/HelloCSharp.cs
@@ -2,44 +2,72 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class HelloCSharp : MonoBehaviour
-{
+public class HelloCSharp: MonoBehaviour {
     // Start is called before the first frame update
     void Start() {
-        
+
         // 형변환 캐스팅
         int height = 170;
-        float heightDetail = 170.3f;
+        float heightDetail = 170.3 f;
 
-        // height = heightDetail;
-        // 자동 형변환        
+        // height = heightDetail; 자동 형변환
         heightDetail = height;
 
         height = (int)heightDetail;
-        
+
         // 조건문 if문
         bool isBoy = true;
 
-        if(isBoy == true) {
+        if (isBoy == true) {
             Debug.Log("나는 남자다");
-        }
-
-        else {
+        } else {
             Debug.Log("나는 여자다");
         }
 
         int love = 60;
 
-        if(love < 50) 
-        {
+        if (love < 50) {
             Debug.Log("배드엔딩");
-        }
-        else{
+        } else {
             Debug.Log("해피엔딩");
         }
-    
+
+        int age = 20;
+
+        if (age >= 20) {
+            Debug.Log("성인");
+        } else {
+            Debug.Log("미성년자");
+
         }
-    
+
+        int year = 2017;
+
+        switch(year)
+        {
+            case 2012:
+                Debug.Log("레미제라블");
+                break;
+
+            case 2015:
+                Debug.Log("러브라이브");
+                break;
+
+            case 2016:
+                Debug.Log("곡성");
+                break;
+
+            case 2017:
+                Debug.Log("트랜스포머5");
+                break;
+
+            default:
+                Debug.Log("해당 없음");
+                break;
+
+
+        }
+
     }
 
 }
