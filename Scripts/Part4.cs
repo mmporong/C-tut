843b9ca1d39d9f282c4558f4b24e0bd2e7f1d55f
mmporong <74720422+mmporong@users.noreply.github.com>
2021-04-12 23:25:17 +0900

    Add files via upload

--- Scripts/Part4.cs
+++ Scripts/Part4.cs
@@ -2,19 +2,47 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+
+public class Test2
+{
+    private int d;
+    public int c;
+    public static int e; // 공유자원, 정적 변수
+
+    public void abc3()
+    {
+
+    }
+
+    private void abc4()
+    {
+
+    }
+}
+
 public class Part4 : MonoBehaviour
 {
-    int a = 5 ; // 멤버 변수, 전역 변수
+    Test2 a1 = new Test2();
+    Test2 a2 = new Test2();
+    Test2 a3 = new Test2();
 
-    // Start is called before the first frame update
+    void abcd()
+    {
+        a1.c = 1;
+        a2.c = 5;
+        a3.c = 10;
+
+        Test2.e = 100;
+
+        print(a1.c);
+        print(a2.c);
+        print(a3.c);
+        print(Test2.e);
+    }
+
     void Start()
     {
-        
+        abcd();
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
 }
