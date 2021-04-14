4aeb4e77b7934cf48fcab883ba1bd6b612cbabc5
mmporong <74720422+mmporong@users.noreply.github.com>
2021-04-14 23:51:19 +0900

    Add files via upload

---
+++ Scripts/Part9.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Part9 : MonoBehaviour
+{
+    int[] exp = new int[5] { 1, 2, 3, 4, 5 };
+
+    // 컬렉션 : 리스트, 큐, 스택. 해시테이블, ㄷ기셔너리, 어레이리스트
+
+    // ArrayList
+
+    ArrayList arrayList = new ArrayList();
+
+
+
+    void Start()
+    {
+        arrayList.Add(1);
+        arrayList.Add(2);
+        arrayList.Add("가나"); 
+        arrayList.Remove("가나");
+
+
+        print(arrayList.Count);  // 5
+
+        for (int i = 0; i < arrayList.Count; i++)
+        {
+            print(arrayList[i]);
+        }
+
+    }
+
+
+}
