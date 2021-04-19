37a66083644f916b12c582f01b3ac67617306318
mmporong <74720422+mmporong@users.noreply.github.com>
2021-04-19 22:20:41 +0900

    Add files via upload

--- Scripts/Part9.cs
+++ Scripts/Part9.cs
@@ -12,7 +12,25 @@ public class Part9 : MonoBehaviour
 
     ArrayList arrayList = new ArrayList();
 
+    // List
 
+    List<int> list = new List<int>(); // 특정 형식만 가능
+
+    // HashTable
+
+    Hashtable hashTable = new Hashtable();
+
+    // Dictionary
+
+    Dictionary<string, int> dictionary = new Dictionary<string, int>();
+
+    // Queue, 선입선출, FIFO, 포션 제작 대기줄 (은행 대기줄)
+
+    Queue<int> queue = new Queue<int>();
+
+    // Stack , 후입선출, LIFO, 요리게임(접시 설거지)
+
+    Stack<int> stack = new Stack<int>();
 
     void Start()
     {
@@ -30,7 +48,39 @@ public class Part9 : MonoBehaviour
             print(arrayList[i]);
         }
 
+        hashTable.Add("천", 1000);
+        hashTable.Add("만", 10000);
+
+        print(hashTable["천"]);
+
+        dictionary.Add("천", 1000);
+
+        print(dictionary["천"]);
+
+        queue.Enqueue(5);
+        queue.Enqueue(10);
+
+        if(queue.Count != 0)
+            print(queue.Dequeue());
+        if (queue.Count != 0)
+            print(queue.Dequeue());
+        if (queue.Count != 0)
+            print(queue.Dequeue());
+
+        stack.Push(1);
+        stack.Push(2);
+        stack.Push(3);
+
+        if(stack.Count != 0)
+            print(stack.Pop());
+        if (stack.Count != 0)
+            print(stack.Pop());
+        if (stack.Count != 0)
+            print(stack.Pop());
+        if (stack.Count != 0)
+            print(stack.Pop());
     }
 
 
 }
+
