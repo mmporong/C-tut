4fcfafbd81e6df9e4561b6b0e5ff4c89e09b1a9a
mmporong <74720422+mmporong@users.noreply.github.com>
2021-05-08 15:08:32 +0900

    Add files via upload

---
+++ Scripts/RectArea.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace ConsoleApp2
+{
+    class RectArea
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("사각형의 너비?");
+            string width = Console.ReadLine();
+            int wid = int.Parse(width);
+
+            Console.WriteLine("사각형의 높이?");
+            string height = Console.ReadLine();
+            int hei = int.Parse(height);
+
+            int area = wid * hei;
+            Console.WriteLine("사각형의 넓이는 : " + area);
+            
+
+        }
+    }
+}
