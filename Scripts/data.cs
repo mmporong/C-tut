f861c6f90d261175de6253eb6504ab5f67679775
mmporong <74720422+mmporong@users.noreply.github.com>
2021-05-10 23:08:28 +0900

    Add files via upload

---
+++ Scripts/data.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Data
+    {
+        static void Main(string[] args)
+        {
+
+            int hp = 100;
+            bool IsAlive = (hp > 0);
+
+
+            while (hp > 50)
+            {
+                hp--;
+                Console.WriteLine(hp);
+
+
+            }
+            bool a = IsAlive;
+            Console.WriteLine(a);
+
+        }
+    }
+}
