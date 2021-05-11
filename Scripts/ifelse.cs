0571cf81fdccdeded520eb596293394fcf5760af
mmporong <74720422+mmporong@users.noreply.github.com>
2021-05-11 23:08:00 +0900

    Add files via upload

---
+++ Scripts/ifelse.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace ConsoleApp5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            // if else
+            // for while
+            // switch
+            // break continue
+
+        }
+    }
+}
